cd4969d120598555cdb3797ff70e0fd51c06e925
ojasvin <ojasvinborawke@gmail.com>
2026-06-10 22:06:25 +0530

    added product class

---
+++ src/classes/Product.java
@@ -0,0 +1,20 @@
+package classes;
+
+import java.util.*;
+
+public class Product {
+    private Map<String,String> data;
+
+    public Product(Map<String,String> data){
+        this.data = data;
+    }
+
+    public String get(String column){
+        return data.get(column);
+    }
+
+    @Override
+    public String toString(){
+        return data.toString();
+    }
+}
