395b9a953eb35f6e8ffb31ee0248fd4592ab378a
ojasvin <ojasvinborawke@gmail.com>
2026-06-11 22:26:47 +0530

    replaced product class with record product

--- src/classes/Product.java
+++ src/classes/Product.java
@@ -2,19 +2,24 @@ package classes;
 
 import java.util.*;
 
-public class Product {
-    private final Map<String,String> data;
 
-    public Product(Map<String,String> data){
-        this.data = data;
-    }
+public record Product(Map<String,String> data){
 
-    public String get(String column){
-        return data.get(column);
-    }
-
-    @Override
-    public String toString(){
-        return data.toString();
-    }
 }
+
+//public class Product {
+//    private final Map<String,String> data;
+//
+//    public Product(Map<String,String> data){
+//        this.data = data;
+//    }
+//
+//    public String get(String column){
+//        return data.get(column);
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return data.toString();
+//    }
+//}
